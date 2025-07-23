3a876c8d60f5250b53aba9f50e93e60d75da2054
Nihal Tariq <nihaltariq55@gmail.com>
2025-07-23 12:54:32 +0500

    Initial commit

--- database.py
+++ database.py
@@ -1,10 +1,12 @@
+import os
+from dotenv import load_dotenv
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
+load_dotenv()
 
-URl_DATABASE = 'postgresql://postgres:1234@localhost:1234/QuizApplication'
-
+URl_DATABASE = os.environ.get('DATABASE_URL')
 engine = create_engine(URl_DATABASE)
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
